6f41c41285468a7e3479a4bd017b99d5dbe1fd93
MedhaVashisth <112161904+MedhaVashisth@users.noreply.github.com>
2022-10-03 17:43:42 +0530

    Add files via upload

---
+++ Advanced Assignment_Python 5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. Multiple inheritance is a feature of some object-oriented computer programming languages in which an object or class can inherit features from more than one parent object or parent class. It is distinct from single inheritance, where an object or class may only inherit from one particular object or class.
+
+2.  According to Wikipedia, “the delegation pattern is an object-oriented design pattern that allows object composition to achieve the same code reuse as inheritance.” If you read that sentence four or five times you might get it but let me try to make it simpler
+
+3. Composition is a concept that models a has a relationship. It enables creating complex types by combining objects of other types. This means that a class Composite can contain an object of another class Component . This relationship means that a Composite has a Component .
+
+4. A bound method is the one which is dependent on the instance of the class as the first argument. It passes the instance as the first argument which is used to access the variables and functions. In Python 3 and newer versions of python, all functions in the class are by default bound methods.
+
+5. This is sometimes misleadingly called private attributes really, it's just a way to localize a name to the class that created it, and does not prevent access by code outside the class. That is, this feature is mostly intended to avoid namespace collisions in instances, not to restrict access to names in general.
